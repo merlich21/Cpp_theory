8ab5e266ace907e168b62c816bc315c0b713545c
Marylin Erlich <merlich@il-g5.msk.21-school.ru>
2022-11-05 23:33:34 +0300

    fix

--- templates/default_type/Vertex.class.hpp
+++ templates/default_type/Vertex.class.hpp
@@ -6,7 +6,7 @@
 /*   By: merlich <merlich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 22:40:47 by merlich           #+#    #+#             */
-/*   Updated: 2022/11/04 23:05:20 by merlich          ###   ########.fr       */
+/*   Updated: 2022/11/05 23:33:04 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ class	Vertex
 
 	public:
 
-		Vertex<T>( T const & x , T const & y, T const & z );
-		~Vertex<T>( void );
+		Vertex<T>( T const & x , T const & y, T const & z ) : _x(x), _y(y), _z(z) {}
+		~Vertex<T>( void ) {}
 
-		T const	getX( void ) const;
-		T const	getY( void ) const;
-		T const	getZ( void ) const;
+		T const	getX( void ) const { return this->_x; }
+		T const	getY( void ) const { return this->_y; }
+		T const	getZ( void ) const { return this->_z; }
 
 	private:
 
@@ -35,11 +35,21 @@ class	Vertex
 		T const	_y;
 		T const	_z;
 
-		Vertex<T>( void );
+		Vertex<T>( void ) {}
 
 };
 
 template< typename T >
-std::ostream &	operator<<( std::ostream & o, Vertex<T> const & v);
+std::ostream &	operator<<( std::ostream & o, Vertex<T> const & v)
+{
+	std::cout.precision( 1 );
+	o << std::setiosflags( std::ios::fixed );
+	o << "Vertex ( ";
+	o << v.getX() << ", ";
+	o << v.getY() << ", ";
+	o << v.getZ();
+	o << " )";
+	return o;
+}
 
 #endif
